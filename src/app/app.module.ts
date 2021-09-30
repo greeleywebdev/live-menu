2f6bde1c950b499c97444777af27bb6367c82544
greeleywebdev <54598615+greeleywebdev@users.noreply.github.com>
2021-09-29 21:24:00 -0400

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  entryComponents: [
+    HeaderComponent
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  exports:  [
+    HeaderComponent
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
